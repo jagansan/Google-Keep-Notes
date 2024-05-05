a4d727954a29fd5a3821dbe1ae0d23050a5453f0
jagansan <jagansanth603@gmail.com>
2024-05-05 13:53:45 +0530

    google keep notes changes

--- src/components/pages/GoogleChat.js
+++ src/components/pages/GoogleChat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,37 +7,44 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
 
 const GoogleChat = () => {
-  return (
-    <div className='google_keep_notes'>
-        <Container>
-            <div className='title___keep___notes '>
-                <Row className='justify-content-md-center'>
-                    <Col md={8}>
-                        <div className='google___keep___elements'>
-                            <div className='chat___title'>
-                                <Form>
-                                    <InputGroup className="mb-3">
-                                        <Form.Control placeholder="Title" aria-label="Recipient's username" aria-describedby="basic-addon2" className='input__module'/>
-                                    </InputGroup>
-                                </Form>                            
+
+    const [typing, settyping] = useState(false);
+    const [title, settitle] = useState('');
+    function titlechanged(e) {
+        settitle(e.target.value);
+    }
+
+    return (
+        <div className='google_keep_notes'>
+            <Container>
+                <div className='title___keep___notes '>
+                    <Row className='justify-content-md-center'>
+                        <Col md={8}>
+                            <div className='google___keep___elements'>
+                                <div className='chat___title'>
+                                    <Form>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control placeholder="Title" onChange={titlechanged} className='input__module'/>
+                                        </InputGroup>
+                                    </Form>                            
+                                </div>
+                                <div className='chat___descr'>
+                                    <Form>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control as="textarea" placeholder="Take a Note..." rows={3} className='input__module'/>
+                                        </InputGroup>
+                                    </Form>  
+                                </div>
+                                <div className='add___note___icon'>
+                                    <div className='icon___style'><FaPlus /></div>
+                                </div>
                             </div>
-                            <div className='chat___descr'>
-                                <Form>
-                                    <InputGroup className="mb-3">
-                                        <Form.Control as="textarea" placeholder="Take a Note..." rows={3} className='input__module'/>
-                                    </InputGroup>
-                                </Form>  
-                            </div>
-                            <div className='add___note___icon'>
-                                <div className='icon___style'><FaPlus /></div>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-        </Container>
-    </div>
-  )
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default GoogleChat
